ccb5925f4b42ac279d32de2292f173e228b23fb2
Al-Amin018555 <alamin15-3799@diu.edu.bd>
2025-05-04 23:45:55 +0600

    created videos section

--- js/video.js
+++ js/video.js
@@ -1,13 +1,23 @@
 // 1. fetch load and show categories on html
 
 // create loadCategories
-const loadCategories = () =>{
+const loadCategories = () => {
     // fetch the data
     fetch('https://openapi.programming-hero.com/api/phero-tube/categories')
-    .then(res => res.json())
-    .then(data => displayCategories(data.categories))
-    .catch(err => console.log(err))
+        .then(res => res.json())
+        .then(data => displayCategories(data.categories))
+        .catch(err => console.log(err))
 };
+
+//load videos
+const loadVideos = () => {
+    fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+        .then(res => res.json())
+        .then(data => displayVideos(data.videos))
+        .catch(err => console.log(err))
+
+}
+
 
 
 // create displayCategories
@@ -21,10 +31,36 @@ const displayCategories = (categories) => {
         const button = document.createElement('button');
         button.classList = "btn";
         button.innerText = item.category;
-        
+
         //adding button to category container
         categoriesContainer.append(button);
     });
 };
 
+//display videos
+const displayVideos = (videos) => {
+    const videosContainer = document.getElementById('videos');
+    videos.forEach(video => {
+        console.log(video)
+        const card = document.createElement('div');
+        card.classList = "card bg-base-100";
+        card.innerHTML = `
+        <figure>
+                 <img
+                  src="${video.thumbnail}"
+                 alt="Shoes" />
+         </figure>
+        <div class="card-body">
+         <h2 class="card-title">Card Title</h2>
+         <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+         <div class="card-actions justify-end">
+        <button class="btn btn-primary">Buy Now</button>
+         </div>
+        </div>
+        `;
+        videosContainer.append(card);
+    })
+};
+
 loadCategories();
+loadVideos()
